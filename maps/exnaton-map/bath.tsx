14bd3c87112fbaef3c5b8abc6fe934c615dcb1ee
Daniel <daniel@exnaton.com>
2022-12-08 17:49:10 +0100

    updates

---
+++ maps/exnaton-map/bath.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="bath" tilewidth="32" tileheight="32" tilecount="180" columns="12">
+ <image source="tilesets/bath.png" width="384" height="480"/>
+</tileset>
---
+++ maps/exnaton-map/fashion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="fashion" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="tilesets/fashion.png" width="768" height="768"/>
+</tileset>
